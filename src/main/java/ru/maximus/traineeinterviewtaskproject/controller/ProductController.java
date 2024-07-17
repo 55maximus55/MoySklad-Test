6d63ac10c5271a647bd6b759fdeb42b49f5ad8c3
55_maximus_55 <lukinosh55@gmail.com>
2024-07-17 06:18:25 +0300

    Error handling

--- src/main/java/ru/maximus/traineeinterviewtaskproject/controller/ProductController.java
+++ src/main/java/ru/maximus/traineeinterviewtaskproject/controller/ProductController.java
@@ -2,11 +2,13 @@ package ru.maximus.traineeinterviewtaskproject.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 import ru.maximus.traineeinterviewtaskproject.Error;
 import ru.maximus.traineeinterviewtaskproject.service.ProductService;
 import ru.maximus.traineeinterviewtaskproject.entity.Product;
 
+import java.io.FileNotFoundException;
 import java.util.Map;
 
 @RestController
@@ -16,11 +18,9 @@ public class ProductController {
     private final ProductService productService = new ProductService();
 
     @GetMapping("{id}")
-    public ResponseEntity<Object> getProduct(@PathVariable long id) {
+    public ResponseEntity<Object> getProduct(@PathVariable long id) throws Exception {
         Product product = productService.getProduct(id);
-        if (product == null) {
-            return new ResponseEntity<>(new Error(HttpStatus.NOT_FOUND.value(), "Product with id " + id + " not found"), HttpStatus.NOT_FOUND);
-        }
+        if (product == null) throw new FileNotFoundException("Product with id " + id + " not found");
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
@@ -31,51 +31,50 @@ public class ProductController {
 
     @PostMapping("create")
     public ResponseEntity<Object> createProduct(@RequestBody Product product) throws Exception {
-        String error = productService.addProduct(product);
-        if (error != null) {
-            throw new Exception(error);
-//            return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), error), HttpStatus.BAD_REQUEST);
-        }
+        productService.addProduct(product);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
     @PutMapping("update")
-    public ResponseEntity<Object> updateProduct(@RequestBody Product updateProduct) {
-        if (updateProduct.getId() == null) return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), "Product id is null"), HttpStatus.BAD_REQUEST);
-        Product product = productService.getProduct(updateProduct.getId());
-        if (product == null) {
-            return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), "Product with id " + updateProduct.getId() + " not found"), HttpStatus.BAD_REQUEST);
-        }
-
-        if (updateProduct.getName() != null && updateProduct.getName().isEmpty()) {
-            return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), "Product name can't be empty"), HttpStatus.BAD_REQUEST);
-        }
-        if (updateProduct.getDescription() != null && updateProduct.getDescription().length() > 4096) {
-            return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), "Product description is longer than 4096 characters"), HttpStatus.BAD_REQUEST);
-        }
-        if (updateProduct.getPrice() != null && updateProduct.getPrice() < 0) {
-            return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), "Product price is negative"), HttpStatus.BAD_REQUEST);
-        }
-
-        if (updateProduct.getName() != null) product.setName(updateProduct.getName());
-        if (updateProduct.getDescription() != null) product.setDescription(updateProduct.getDescription());
-        if (updateProduct.getPrice() != null) product.setPrice(updateProduct.getPrice());
-        if (updateProduct.getInStock() != null) product.setInStock(updateProduct.getInStock());
-
+    public ResponseEntity<Object> updateProduct(@RequestBody Product updateProduct) throws Exception {
+        Product product = productService.updateProduct(updateProduct);
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
     @DeleteMapping("delete")
-    public ResponseEntity<Object> deleteProduct(@RequestBody Product product) {
-        if (product.getId() == null) {
-            return new ResponseEntity<>(new Error(HttpStatus.BAD_REQUEST.value(), "Product id is null"), HttpStatus.BAD_REQUEST);
-        }
-        if (productService.getProduct(product.getId()) == null) {
-            return new ResponseEntity<>(new Error(HttpStatus.NOT_FOUND.value(), "Product with id " + product.getId() + " not found"), HttpStatus.NOT_FOUND);
-        }
-
+    public ResponseEntity<Object> deleteProduct(@RequestBody Product product) throws FileNotFoundException {
         productService.deleteProduct(product.getId());
-        return new ResponseEntity<>(product, HttpStatus.OK);
+        return new ResponseEntity<>(Map.of("status", HttpStatus.OK.value()), HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(Exception.class)
+    public Map<String, Object> handleException(Exception ex) {
+        return Map.of(
+                "message", ex.getMessage(),
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Map<String, Object> handleException(IllegalArgumentException ex) {
+        return Map.of(
+                "message", ex.getMessage(),
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(FileNotFoundException.class)
+    public Map<String, Object> handleException(FileNotFoundException ex) {
+        return Map.of(
+                "message", ex.getMessage(),
+                "status", HttpStatus.NOT_FOUND.value(),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase()
+        );
     }
 
 }
--- src/main/java/ru/maximus/traineeinterviewtaskproject/service/ProductService.java
+++ src/main/java/ru/maximus/traineeinterviewtaskproject/service/ProductService.java
@@ -1,7 +1,12 @@
 package ru.maximus.traineeinterviewtaskproject.service;
 
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import ru.maximus.traineeinterviewtaskproject.Error;
 import ru.maximus.traineeinterviewtaskproject.entity.Product;
 
+import java.io.FileNotFoundException;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -18,21 +23,20 @@ public class ProductService {
         return products.values();
     }
 
-    public String addProduct(Product product) {
+    public void addProduct(Product product) {
         if (product.getName() == null || product.getName().isEmpty()) {
-            return "Product name can't be empty";
+            throw new IllegalArgumentException("Product name cannot be empty");
         }
-
         if (product.getDescription() == null) {
             product.setDescription("");
         } else if (product.getDescription().length() > 4096) {
-            return "Product description is longer than 4096 characters";
+            throw new IllegalArgumentException("Product description cannot be longer than 4096");
         }
 
         if (product.getPrice() == null) {
             product.setPrice(0.0);
         } else if (product.getPrice() < 0) {
-            return "Product price can't be negative";
+            throw new IllegalArgumentException("Product price cannot be negative");
         }
 
         if (product.getInStock() == null) {
@@ -41,11 +45,32 @@ public class ProductService {
 
         product.setId(nextProductId++);
         products.put(product.getId(), product);
-        return null;
     }
 
-    public void deleteProduct(long id) {
-        products.remove(id);
+    public Product updateProduct(Product updateProduct) throws Exception {
+        if (updateProduct.getId() == null) throw new BadRequestException("Product id cannot be null");
+        Product product = getProduct(updateProduct.getId());
+        if (product == null) throw new FileNotFoundException("Product with id " + updateProduct.getId() + " not found");
+
+        if (updateProduct.getName() != null && !updateProduct.getName().isEmpty()) {
+            product.setName(updateProduct.getName());
+        }
+        if (updateProduct.getDescription() != null && updateProduct.getDescription().length() <= 4096) {
+            product.setDescription(updateProduct.getDescription());
+        }
+        if (updateProduct.getPrice() != null && !(updateProduct.getPrice() < 0)) {
+            product.setPrice(updateProduct.getPrice());
+        }
+        if (updateProduct.getInStock() != null) product.setInStock(updateProduct.getInStock());
+
+        return product;
+    }
+
+    public void deleteProduct(long id) throws FileNotFoundException {
+        if (products.containsKey(id))
+            products.remove(id);
+        else
+            throw new FileNotFoundException("Product with id " + id + " not found");
     }
 
 }
